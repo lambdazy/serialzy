18044f1849dcb1ff082912d9e87fcb8d4aa7eeab
NataliaPonomarevaMM <natpon12@gmail.com>
2023-01-31 12:22:49 +0100

    Added XGBoost serializer

---
+++ serialzy/serializers/xgboost.py
@@ -0,0 +1,32 @@
+from typing import BinaryIO, Callable, Type, Union, Any, Optional
+
+from packaging import version  # type: ignore
+
+from serialzy.serializers.base_model import ModelBaseSerializer, serialize_to_file, deserialize_from_file
+
+
+# noinspection PyPackageRequirements
+class XGBoostSerializer(ModelBaseSerializer):
+    def __init__(self):
+        super().__init__("xgboost", __name__)
+
+    def _serialize(self, obj: Any, dest: BinaryIO) -> None:
+        serialize_to_file(dest, lambda x: obj.save_model(x))
+
+    def _deserialize(self, source: BinaryIO, schema_type: Type, user_type: Optional[Type] = None) -> Any:
+        self._check_types_valid(schema_type, user_type)
+        import xgboost  # type: ignore
+
+        def load_model(filename):
+            model = xgboost.Booster({'nthread': 4})
+            model.load_model(filename)
+            return model
+
+        return deserialize_from_file(source, load_model)
+
+    def supported_types(self) -> Union[Type, Callable[[Type], bool]]:
+        import xgboost as xgb  # type: ignore
+        return lambda t: t in [xgb.XGBRanker, xgb.XGBRegressor, xgb.XGBClassifier]
+
+    def data_format(self) -> str:
+        return "xgb"
---
+++ tests/rich_env/serializers/test_xgboost.py
@@ -0,0 +1,44 @@
+import xgboost
+# noinspection PyPackageRequirements
+import numpy as np
+# noinspection PyPackageRequirements
+from sklearn import datasets
+# noinspection PyPackageRequirements
+from sklearn.model_selection import train_test_split
+
+from serialzy.serializers.xgboost import XGBoostSerializer
+from tests.rich_env.serializers.test_base_model import ModelBaseSerializerTests
+
+
+class XGBoostModelSerializationTests(ModelBaseSerializerTests):
+    def setUp(self):
+        self.initialize("xgboost")
+
+    def test_serialization(self):
+        dataset = datasets.load_wine()
+        x = dataset.data
+        y = dataset.target
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.30)
+        dt = xgboost.DMatrix(x_test)
+
+        model = xgboost.XGBClassifier()
+        model.fit(x_train, y_train)
+
+        deserialized_model = self.base_test(model, XGBoostSerializer)
+        self.assertTrue(np.allclose(model.predict_proba(x_test), deserialized_model.predict(dt)))
+
+    def test_schema(self):
+        self.base_schema('xgb', xgboost.XGBRanker)
+
+    def test_resolve(self):
+        self.base_resolve('xgb', xgboost.XGBClassifier)
+
+    def test_invalid_types(self):
+        dataset = datasets.load_wine()
+        x = dataset.data
+        y = dataset.target
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.30)
+
+        model = xgboost.XGBClassifier()
+        model.fit(x_train, y_train)
+        self.base_invalid_types(model, xgboost.XGBRegressor)
